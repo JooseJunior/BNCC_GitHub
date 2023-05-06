d062356b710443af68a568e1d60dd49144409c5f
Vinicius51 <contato.viniciusdanielmonteiro@gmail.com>
2023-05-06 18:53:16 -0400

    Iniciando o models/Comentario

--- src/models/Comentario.js
+++ src/models/Comentario.js
@@ -1,1 +1,30 @@
-//Novo
+const moongose = require('moongose');
+
+const ComentarioSchema = new mongoose.Schema({
+
+    text:{
+        type: String,
+        required: true
+    },
+
+    user:{
+        type: String,
+        required: true
+    },
+
+    post: {
+        type:moongose.Schema.Types.ObjectId,
+        ref: 'Post',
+        required:true
+    },
+
+    liked: {
+        type: Boolean,
+        default: false,
+    },
+
+    createdAT: {
+        type: Date, 
+        default: Date.now
+    }
+})
