2d4a5e9f58a150b0531b4e0dfff3d3a8ab8f0164
Vinicius51 <contato.viniciusdanielmonteiro@gmail.com>
2023-05-06 21:09:44 -0400

    Finalizando routes, models e controller  comentario

--- src/routes/ComentarioRoutes.js
+++ src/routes/ComentarioRoutes.js
@@ -1,1 +1,16 @@
 //Novo
+import express from 'express';
+const router = express.Router();
+import ComentarioController from '../controllers/ComentarioController';
+
+router.post('/', ComentarioController.criarComentario);
+
+router.get('/postId', ComentarioController.listarComentarios);
+
+router.put('/comentarioId', ComentarioController.atualizarComentario);
+
+router.delete('/:comentarioId', ComentarioController.excluirComentario);
+
+router.post('comentarios/comentarioId/like', ComentarioController.darLikeComentario);
+
+export default router;
