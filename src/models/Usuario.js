b700ab63f6c2a05eae19cf1bf920bb485ad4f8d2
Jose Junior <josejuniorvha@gmail.com>
2023-05-10 19:37:33 -0300

    Sincronização pasta Seeds

--- src/models/Usuario.js
+++ src/models/Usuario.js
@@ -1,25 +1,28 @@
 import mongoose from "mongoose";
 import mongoosePaginate from 'mongoose-paginate';
 
+// Schema -> construtor para instanciar objetos no banco
+
 const usuarioSchema = new mongoose.Schema({
     nome: { type: String, required: true, min: 6, max: 255, trim: true},
-    email: {
+    email: { 
         type: String, required: true, unique: true,
+        //Regex => Expressão Regular
         match: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
     },
     senha: { type: String, minlength: 8, trim: true, required: true, select: false },
     ativo: { type: Boolean, default: false },
-    rotas: [
-        {
-            _id: { type: mongoose.Schema.Types.ObjectId, ref: 'rotas' },    //ID da rota
-            rota: { type: String, trim: true, required: true },             //Nome da rota
-            verbo_get: { type: Boolean },
-            verbo_put: { type: Boolean },
-            verbo_patch: { type: Boolean },
-            verbo_delete: { type: Boolean },
-            verbo_post: { type: Boolean }
-        }
-    ]
+    // rotas: [
+    //     {
+    //         _id: { type: mongoose.Schema.Types.ObjectId, ref: 'rotas' },    //ID da rota
+    //         rota: { type: String, trim: true, required: true },             //Nome da rota
+    //         verbo_get: { type: Boolean },
+    //         verbo_put: { type: Boolean },
+    //         verbo_patch: { type: Boolean },
+    //         verbo_delete: { type: Boolean },
+    //         verbo_post: { type: Boolean }
+    //     }
+    // ]
 }
 );
 
