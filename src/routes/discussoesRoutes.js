81632913117423f901eec844abb98b742073347b
Guilherme Rodrigues <guylherme_rodrygues@hotmail.com>
2023-05-11 15:53:31 -0300

    Criei a rota de discussões e modifiquei o arquivo para o plural

---
+++ src/routes/discussoesRoutes.js
@@ -0,0 +1,17 @@
+import express from "express";
+import DiscussoesController from "../controller/discussoesController.js";
+import AuthMidleware from "../middlewares/AuthMidleware.js";
+
+
+const router = express.Router();
+
+
+router
+  .get("/discussoes", DiscussoesController.listarDiscussoes)
+  .get("/discussoes/:id", DiscussoesController.listarDiscussoesPorId)
+  .post("/discussoes",AuthMidleware, DiscussoesController.cadastrarDiscussoes)
+  .put("/discussoes/:id",AuthMidleware, DiscussoesController.atualizarDiscussoes)
+  .delete("/discussoes/:id",AuthMidleware, DiscussoesController.excluirDiscussoes)
+  .patch("/discussoes/:id",AuthMidleware, DiscussoesController.atualizarDiscussoes)
+
+export default router;
