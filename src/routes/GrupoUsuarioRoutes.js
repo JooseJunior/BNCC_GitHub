88d887f649c7fdfae16b05a78dd76fa11aacc55d
Luccas <luccas.rossarolla@estudante.ifro.edu.br>
2023-05-17 22:02:16 -0400

    Adicionar versionKey

--- src/routes/GrupoUsuarioRoutes.js
+++ src/routes/GrupoUsuarioRoutes.js
@@ -9,4 +9,9 @@ router
     .get("/grupos", GrupoController.listarGrupo)
     .get("/grupos/:id", GrupoController.listarGrupoPorId)
     
+const grupoSchema = new mongoose.Schema({
+}, { versionKey: false });
+      
+const Grupo = mongoose.model("Grupo", grupoSchema);
+      
 export default router;
