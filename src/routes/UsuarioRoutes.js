a1928f59fb20feccded51c84fd0d2b1f511f2b6a
Vinicius51 <contato.viniciusdanielmonteiro@gmail.com>
2023-05-26 21:53:48 -0400

    Iniciando testes

--- src/routes/UsuarioRoutes.js
+++ src/routes/UsuarioRoutes.js
@@ -1,7 +1,7 @@
 //JOSE
 import express from "express";
 import UsuarioController from "../controllers/UsuarioController.js";
-import mongoose from "mongoose";
+
 // import AuthMiddleware from "../middlewares/AuthMiddleware.js";
 import mongoose from "mongoose";
 
--- src/tests/models/LinhaTempoRoutes.spec.js
+++ src/tests/models/LinhaTempoRoutes.spec.js
@@ -1,1 +1,26 @@
 //VINICIUS
+import { describe, expect, it, jest,} from '@jest/globals';
+import LinhaTempoController from "../../controllers/LinhaTempoController.js";
+import linhatempos from "../../models/LinhaTempo.js";
+
+describe('Teste de unidade do documento', () => {
+    afterEach(() => jest.clearAllMocks());
+  
+    it('Deve instanciar um novo documento corretamente', () => {
+      const Docsexemplo = new Date("2022-08-29");
+      const Documento = {
+        titulo: 'Projeto de Lei n. 13',
+        conteudo: 'O Projeto de Lei 13/22 obriga as empresas de transporte de passageiros (terrestre, aéreo ou fluvial) a fornecer rastreamento dos animais de estimação transportados.',
+        criando_em: Docsexemplo
+      };
+  
+      const documento = new linhatempos(Documento);
+      expect(documento).toEqual(expect.objectContaining(Documento));
+      expect(documento).toHaveProperty('titulo', 'Projeto de Lei n. 13');
+    });
+
+    
+  });
+
+
+ 
