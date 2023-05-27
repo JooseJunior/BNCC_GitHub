7b82e9b92c205f04456b40c37b093ffb76ed0c36
Vinicius51 <contato.viniciusdanielmonteiro@gmail.com>
2023-05-26 22:25:03 -0400

    Realizando testes

--- src/tests/models/LinhaTempoRoutes.spec.js
+++ src/tests/models/LinhaTempoRoutes.spec.js
@@ -19,7 +19,25 @@ describe('Teste de unidade do documento', () => {
       expect(documento).toHaveProperty('titulo', 'Projeto de Lei n. 13');
     });
 
-    
+    it('Deve retornar um cadastro de documento simulado com Mock', () => {
+        const Docsexemplo = new Date("2022-08-29");
+        const Documentos = new linhatempos({
+          titulo: 'Projeto de Lei n. 13',
+          conteudo: 'O Projeto de Lei 13/22 obriga as empresas de transporte de passageiros (terrestre, aéreo ou fluvial) a fornecer rastreamento dos animais de estimação transportados.',
+          criando_em: Docsexemplo
+        });
+      
+        LinhaTempoController.AdicionarDocumento = jest.fn().mockReturnValue(Documentos);
+      
+        const retorno = LinhaTempoController.AdicionarDocumento();
+      
+        expect(retorno).toEqual(expect.objectContaining({
+          criando_em: expect.any(Date),
+          ...Documentos,
+        }));
+      
+        expect(LinhaTempoController.AdicionarDocumento).toBeCalledTimes(1);
+      });
   });
 
 
